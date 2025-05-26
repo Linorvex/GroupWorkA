bafef1a2c22c802b94a54b701f03fdcdda3e32d8
Gvantsa-Tabunidze <gvantsaletta9@gmail.com>
2025-05-26 17:54:10 +0400

    ingredients page added

---
+++ src/Pages/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddCoffee = () => {
+  return (
+    <div className='mainContent'>
+        <h1>Hey! It's an Add Coffee page</h1>
+    </div>
+  )
+}
+
+export default AddCoffee
---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='mainContent'>
+        <h1>Hey it's a Dashboard page, here</h1>
+    </div>
+  )
+}
+
+export default Dashboard
